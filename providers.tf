f97c42efe710cb1776170af161cd0c40908f05b9
unknown <visheshjimishra@gmail.com>
2022-08-18 14:35:45 +0530

    adding repo

---
+++ providers.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+
+region = "us-east-1"
+access_key = "AKIA4AGIOPUQLEIZKMGU"
+secret_key = "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"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "javahome-tf-1212"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
