4d1aa5c8f1995a996c11b8178ecdccdd9c2e142a
mvishesh06 <111462046+mvishesh06@users.noreply.github.com>
2022-08-18 15:17:57 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,7 +6,13 @@ region = "${var.region}"
 terraform {
   backend "s3" {
     bucket = "javahome-tf-1212"
+    profile = "vishesh"
     key = "terraform.tfstate"
     region = "us-east-1"
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
 }
